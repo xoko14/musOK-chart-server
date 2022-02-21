58433884a939eb489f623dc3d578ae6d8edf82c6
xoko14 <xoquinperezb@gmail.com>
2022-02-21 18:10:27 +0100

    dotemv to configure db connection

--- db/database.py
+++ db/database.py
@@ -1,8 +1,17 @@
+import os
+import dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/rgdb"
+dotenv.load_dotenv()
+
+DB_USER=os.environ.get("DB_USER")
+DB_PASSWORD=os.environ.get("DB_PASSWORD")
+DB_LOCATION=os.environ.get("DB_LOCATION")
+DB_NAME=os.environ.get("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_LOCATION}/{DB_NAME}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
