73bfcb8c5447e35538d9952f5d3dcf3c1279a75c
Xoquín <xoquinperezb@gmail.com>
2022-04-08 08:40:02 +0200

    remove unnecessary authentification

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 @app.get("/users/{user_id}/uploaded", response_model=List[schemas.Song], tags=["users"])
-def read_current_user_uploaded(user_id: int, skip: int = 0, limit: int = 100, current_user: schemas.User = Depends(get_current_user), db: Session = Depends(get_db)):
+def read_current_user_uploaded(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
