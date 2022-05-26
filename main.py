41e7172587ec734a1c37de964e7640e868ee1e32
xoko14 <xoquinperezb@gmail.com>
2022-05-26 19:49:24 +0200

    bug fixes

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def get_current_user_optional(token: str = Depends(oauth2_scheme_optional)
             raise credentials_exception
         token_data = schemas.TokenData(username=username)
     except JWTError:
-        raise credentials_exception
+        return None
     user = crud.get_user_by_username(db, username=token_data.username)
     if user is None:
         raise credentials_exception
